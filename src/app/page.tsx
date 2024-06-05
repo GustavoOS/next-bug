9c4bc314ea4f40c315f5f3ed089ff4e840ff6ace
Souza Gustavo (BD/SWD-FSA1) <sgz2ca@bosch.com>
2024-06-05 13:14:16 -0300

    show anchor download bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,11 @@
 "use client"
 
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { type BaseSyntheticEvent } from "react";
-
-const fileschema = z.object({
-  forwardFiles: z
-  .instanceof(FileList)
-  .optional(),
-})
-
-type fileform = z.infer<typeof fileschema>
-
-
 
 export default function HomePage() {
-  const {
-    register,
-    handleSubmit
-  } = useForm<fileform>({
-    resolver: zodResolver(fileschema)
-  })
-
-  function onSubmit(data: fileform , e?: BaseSyntheticEvent) {
-    e?.preventDefault()
-    console.log(data)
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="file" {...register("forwardFiles")} multiple />
-      <button type="submit">Submit</button>
-      </form>
+      <a className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded" href="https://images.unsplash.com/photo-1716724854567-9ec995836d19" download>Download</a>
     </main>
-  );
+  )
 }
