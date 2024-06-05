6d5961826bda1084cd4d4c32559a59f4f484473c
Souza Gustavo (BD/SWD-FSA1) <sgz2ca@bosch.com>
2024-06-05 13:17:58 -0300

    add file name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <a className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded" href="https://images.unsplash.com/photo-1716724854567-9ec995836d19" download>Download</a>
+      <a className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded" href="https://images.unsplash.com/photo-1716724854567-9ec995836d19"
+       download="photo.jpg">Download</a>
     </main>
   )
 }
